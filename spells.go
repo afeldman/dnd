72dde21ebd1a5c3f24e622ea37954fc805575755
AntonFeldmann <anton.feldmann@gmail.com>
2017-08-05 16:00:43 +0900

    gabs

--- spells.go
+++ spells.go
@@ -1,68 +1,27 @@
 package main
 
 import (  
-    "encoding/json"
     "fmt"
     "io/ioutil"
     "os"
+    "github.com/Jeffail/gabs"
 )
-
-type component struct {
-     material bool
-     raw string
-     somatic bool
-     verbal bool
-}
-
-type Spell struct {
-     casting_time string
-     classes []string
-     components component
-     description string
-     duration string
-     level string
-     rang string `json:"range"`
-     ritual bool
-     school string
-     tags []string
-     typ string `json:"type"`
-}
-
-func (s Spell) toString() string {
-    return toJson(s)
-}
-
-func toJson(s interface{}) string {
-    bytes, err := json.Marshal(s)
-    if err != nil {
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
-
-    return string(bytes)
-}
 
 func main() {
 
-    spells := getSpells(os.Args[1])
-    for _, s := range spells {
-        fmt.Println(s.toString())
-    }
-    fmt.Println(toJson(spells))	
-}
-
-func getSpells(filename string) []Spell {
-    raw, err := ioutil.ReadFile(filename)
-
+    raw, err := ioutil.ReadFile(os.Args[1])
     if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
     }
 
-    var c []Spell
-    json.Unmarshal(raw, &c)
+    s := string(raw)
 
-        fmt.Println(c)	
+   jsonParsed, err := gabs.ParseJSON([]byte(s))
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
 
-    return c
+    fmt.Println(jsonParsed.StringIndent("", "  "))
 }
