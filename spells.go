f9aefee4e3c9613523ed11fbf3652fc9688b7b01
AntonFeldmann <anton.feldmann@gmail.com>
2017-08-05 19:15:57 +0900

    save json function

--- spells.go
+++ spells.go
@@ -1,44 +1,89 @@
 package main
 
 import (
-    "fmt"
+    "log"
     "os"
     "io/ioutil"
+    "fmt"
     "encoding/json"
+    "sort"
 )
 
-/*type component struct {
-     material bool	`json:"material"`
-     materials []string `json:"materials_needed"`
-     raw      string  	`json:"raw"`
-     somatic  bool    	`json:"somatic"`
-     verbal   bool    	`json:"verbal"`
+type Component struct {
+     Material bool	`json:"material,omitempty"`
+     Materials []string `json:"materials_needed,omitempty"`
+     Raw      string  	`json:"raw,omitempty"`
+     Somatic  bool    	`json:"somatic,omitempty"`
+     Verbal   bool    	`json:"verbal,omitempty"`
 }
 
 type Spell struct {
-     casting_t 	  string	`json:"casting_time"`
-     classes 	  []string	`json:"classes"`
-     components   component	`json:"components"`
-     description  string	`json:"description"`
-     duration 	  string	`json:"duration"`
-     level 	  string	`json:"level"`
-     rang 	  string	`json:"range"`
-     ritual 	  bool		`json:"ritual"`
-     school 	  string	`json:"school"`
-     tags 	  []string	`json:"tags"`
-     typ 	  string	`json:"type"`
+     Casting_t 	  string	`json:"casting_time,omitempty"`
+     Classes 	  []string	`json:"classes,omitempty"`
+     Components   Component	`json:"components,omitempty"`
+     Description  string	`json:"description,omitempty"`
+     Duration 	  string	`json:"duration,omitempty"`
+     Level 	  string	`json:"level,omitempty"`
+     Range 	  string	`json:"range,omitempty"`
+     Ritual 	  bool		`json:"ritual,omitempty"`
+     School 	  string	`json:"school,omitempty"`
+     Tags 	  []string	`json:"tags,omitempty"`
+     Type 	  string	`json:"type,omitempty"`
+     Name 	string		`json:"name,omitempty"`
+}
+
+type Spells []Spell
+
+func (slice Spells) Len() int {
+	return len(slice)
+}
+
+func (slice Spells) Less(i, j int) bool {
+	return slice[i].Name < slice[j].Name;
+}
+
+func (slice Spells) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+
+func (s Spells) save_JSON(path string){
+
+     if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir(path, 0755)
+		} else {
+			log.Println(err)
+		}
+	}
+
+	b, err := json.Marshal(s)
+	if err != nil { log.Println(err) }
+
+	ioutil.WriteFile(path, b, 0644) 
 }
 
 func main() {
 
-    file, e := ioutil.ReadFile(os.Args[1])
-    if e != nil {
-        fmt.Printf("File error: %v\n", e)
-        os.Exit(1)
-    }
-    fmt.Printf("%s\n", string(file))
+     file, err := ioutil.ReadFile(os.Args[1])
+     if err != nil {
+             log.Fatal(err)
+     }
 
-    var spells []Spell 
-    json.Unmarshal(file, &spells)
-    fmt.Printf("Results: %v\n", spells[0])
-}*/
+     var spells Spells
+     err = json.Unmarshal(file, &spells)
+     if err != nil {
+             log.Fatal(err)
+     }
+    
+    sort.Sort(spells)
+/*    fmt.Println("\nSorted")
+    for i, c := range spells {
+        fmt.Println(i, c.Name)
+    }*/
+
+    b, _ := json.Marshal(spells)
+    // Convert bytes to string.
+    s := string(b)
+    fmt.Println(s)
+}
