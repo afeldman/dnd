47a0ce9ba89b6b7b7c03b22709f7fc8ef3065433
AntonFeldmann <anton.feldmann@gmail.com>
2017-08-05 17:50:54 +0900

    try automatic json reading

--- spells.go
+++ spells.go
@@ -1,27 +1,44 @@
 package main
 
-import (  
+import (
     "fmt"
-    "io/ioutil"
     "os"
-    "github.com/Jeffail/gabs"
+    "io/ioutil"
+    "encoding/json"
 )
+
+/*type component struct {
+     material bool	`json:"material"`
+     materials []string `json:"materials_needed"`
+     raw      string  	`json:"raw"`
+     somatic  bool    	`json:"somatic"`
+     verbal   bool    	`json:"verbal"`
+}
+
+type Spell struct {
+     casting_t 	  string	`json:"casting_time"`
+     classes 	  []string	`json:"classes"`
+     components   component	`json:"components"`
+     description  string	`json:"description"`
+     duration 	  string	`json:"duration"`
+     level 	  string	`json:"level"`
+     rang 	  string	`json:"range"`
+     ritual 	  bool		`json:"ritual"`
+     school 	  string	`json:"school"`
+     tags 	  []string	`json:"tags"`
+     typ 	  string	`json:"type"`
+}
 
 func main() {
 
-    raw, err := ioutil.ReadFile(os.Args[1])
-    if err != nil {
-        fmt.Println(err.Error())
+    file, e := ioutil.ReadFile(os.Args[1])
+    if e != nil {
+        fmt.Printf("File error: %v\n", e)
         os.Exit(1)
     }
+    fmt.Printf("%s\n", string(file))
 
-    s := string(raw)
-
-   jsonParsed, err := gabs.ParseJSON([]byte(s))
-    if err != nil {
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
-
-    fmt.Println(jsonParsed.StringIndent("", "  "))
-}
+    var spells []Spell 
+    json.Unmarshal(file, &spells)
+    fmt.Printf("Results: %v\n", spells[0])
+}*/
